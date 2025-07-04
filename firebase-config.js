43c84fcb57c009d3aa17d9ddac25846b4ed6a547
eldani84 <danieleberhardt1984@gmail.com>
2025-07-04 10:41:28 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
   apiKey: "AIzaSyCP8o-aD7kqj7eqnziLSkDyLdek4AluV48",
   authDomain: "ranking-alcoholico.firebaseapp.com",
-  databaseURL: "https://ranking-alcoholico-default-rtdb.firebaseio.com",  // <--- importante
+  databaseURL: "https://ranking-alcoholico-default-rtdb.firebaseio.com",
   projectId: "ranking-alcoholico",
-  storageBucket: "ranking-alcoholico.firebasestorage.app",
+  storageBucket: "ranking-alcoholico.appspot.com",
   messagingSenderId: "852318834036",
   appId: "1:852318834036:web:169e1c4afbfa1627353021"
 };
