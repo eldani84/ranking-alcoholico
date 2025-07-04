674282052e4eef1cf51d357f27624cfb10b30df6
eldani84 <danieleberhardt1984@gmail.com>
2025-07-04 09:54:08 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,11 @@
-// Configuración Firebase completa con databaseURL y sin imports
-
 const firebaseConfig = {
   apiKey: "AIzaSyCP8o-aD7kqj7eqnziLSkDyLdek4AluV48",
   authDomain: "ranking-alcoholico.firebaseapp.com",
-  databaseURL: "https://ranking-alcoholico-default-rtdb.firebaseio.com",  // **Agregado**
+  databaseURL: "https://ranking-alcoholico-default-rtdb.firebaseio.com",  // <--- importante
   projectId: "ranking-alcoholico",
   storageBucket: "ranking-alcoholico.firebasestorage.app",
   messagingSenderId: "852318834036",
   appId: "1:852318834036:web:169e1c4afbfa1627353021"
 };
 
-// Inicializa Firebase sin usar imports, con el SDK compat cargado por script
 firebase.initializeApp(firebaseConfig);
